081cdacaf31815465337286e1155ea91e68db5c6
Miri Kuskina <miri86kn@gmail.com>
2014-05-22 15:53:06 +0300

    more params

--- Common/src/Entities/Game2048Settings.java
+++ Common/src/Entities/Game2048Settings.java
@@ -2,4 +2,5 @@ package Entities;
 
 public class Game2048Settings {
 	public static final int EMPTY_CELL=0; //empty cell value
+	public static final int FLAG_STOP_SOLVING = -1; //flag indication stop solving process
 }
